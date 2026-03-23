53292a44fc8e7de6c646ac0c28318f8809f17623
ricardomoraisp <ricardompistori@gmail.com>
2026-03-23 20:28:03 -0300

    s

--- Similaridade.cpp
+++ Similaridade.cpp
@@ -2,12 +2,10 @@
 #include "ListaCompras.h"
 #include <stdlib.h>
 
-//ENTENDER PORQUE ESSA BOMBA NÃO FUNCIONA
-
 int main(){
 
-// Não chamou a função, tá acessando lixo de memória
-    Dados dados_compra;
+    Dados dados;
+	ListaCompras(dados);
  
  int *Matriz_compras = (int*)malloc( sizeof(int) * ( dados.index_client.size() ) * ( dados.index_product.size() ) );
  int *Matriz_compras_transposta = (int*)malloc( sizeof(int) * ( dados.index_product.size() ) * ( dados.index_client.size() ) );
