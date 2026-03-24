6e86b4cb40f0e88e529277ec34aff3a5e5a26da7
Victor-Vasconcelos1106 <skullplays2019@gmail.com>
2026-03-24 08:13:18 -0300

    correção do acesso às matrizes
    Corrected matrix access for similarity calculations.

--- Similaridade.cpp
+++ Similaridade.cpp
@@ -68,8 +68,8 @@ for(i = 0; i < dados.index_client.size(); i++)
  	
        for (j = 0 ; j < dados.index_client.size();j++)
 	   {
-	   
-	   		*( Matriz_similaridade + (i*j) ) = 1 - (*( Matriz_similaridade + (i*j) )) / ( dados.compras_cliente[i].size() );
+	   // atualizei o acesso, estava errado antes
+	   		*( Matriz_similaridade + ( i * dados.index_client.size() + j ) ) = 1 - (*( Matriz_similaridade + ( i * dados.index_client.size() + j ) )) / ( dados.compras_cliente[i].size() );
 	   
 	   }
 	   
@@ -138,8 +138,9 @@ for(i = 0; i < dados.index_client.size(); i++)
  	
        for (j = 0 ; j < dados.index_client.size();j++)
 	   {
-	   
-	   		*( Matriz_similaridade + (i*j) ) = 1 - (*( Matriz_similaridade + (i*j) )) / ( dados.compras_cliente[i].size() );
+	   // estava acessando a memória errado 
+		// atualizei o acesso da matriz
+	   		*( Matriz_similaridade + ( i * dados.index_client.size() + j ) ) = 1 - (*( Matriz_similaridade + ( i * dados.index_client.size() + j ) )) / ( dados.compras_cliente[i].size() );
 	   
 	   }
 	   
